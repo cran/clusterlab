26348fcc5d13bc491ba4a0bf6ef5ec40d2802e05
Christopher R John <chris.r.john86@gmail.com>
2018-08-28 11:04:27 +0000

    version 0.0.2.3

--- inst/doc/introduction.R
+++ inst/doc/introduction.R
@@ -48,15 +48,12 @@ synthetic <- clusterlab(centers=5,r=7,sdvec=c(2,2,2,2,2),
 library(clusterlab)
 synthetic <- clusterlab(centers=5,r=7,sdvec=c(6,6,6,6,6),   
                         alphas=c(2,2,2,2,2),centralcluster=FALSE,   
-                        numbervec=c(50,50,50,50),rings=5,ringalphas=c(2,4,6,8,10,12),
-                        seed=123) # for a six cluster solution)
+                        numbervec=c(50,50,50,50),rings=5,ringalphas=c(2,4,6,8,10,12), 
+                        ringthetas = c(30,90,180,0,0,0), seed=123) # for a six cluster solution)
 
 ## ----fig.width=6,fig.height=6--------------------------------------------
 library(clusterlab)
-synthetic <- clusterlab(centers=5,r=7,sdvec=c(6,6,6,6,6),   
-                        alphas=c(2,2,2,2,2),centralcluster=FALSE,   
-                        numbervec=c(50,50,50,50),rings=5,ringalphas=c(2,4,6,8,10,12), 
-                        ringthetas = c(30,90,180,0,0,0), seed=123) # for a six cluster solution)
+synthetic <- clusterlab(mode='random',centers=12)
 
 ## ----fig.width=6,fig.height=6--------------------------------------------
 head(synthetic$identity_matrix)
